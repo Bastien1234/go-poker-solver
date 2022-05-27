d6f8cd854a3f2318ab00f85ad07b512279e4cd83
BastienC___ <bastien.clementi@gmail.com>
2022-05-27 13:53:42 +0200

    ranges...

--- pkg/ranges/ranges.go
+++ pkg/ranges/ranges.go
@@ -2,7 +2,7 @@ package ranges
 
 func RangeToList(matrix [][]int, pctToKeep int) [][]string {
 	var vectorToReturn [][]string = make([][]string)
-	mapMatrixToCards = map[int]string{
+	mapMatrixToCards := map[int]string{
 		0:  "A",
 		1:  "K",
 		2:  "Q",
@@ -27,6 +27,58 @@ func RangeToList(matrix [][]int, pctToKeep int) [][]string {
 		{"d", "c"},
 		{"d", "s"},
 		{"c", "s"},
+	}
+
+	for i := 0; i< 13; i++ {
+		for j := 0; j<13; j++ {
+			// case pair : i = j
+			if i == j {
+				if matrix[i][j] > 0 {
+					for k := 0; k<(matrix[i][j] / 10); k++ {
+						for _, colorCombo := range allColorsCombo {
+							card1 := mapMatrixToCards[i] + colorCombo[0]
+							card2 := mapMatrixToCards[i] + colorCombo[1]
+							handToAdd := []string {card1, card2}
+							vectorToReturn = append(vectorToReturn, handToAdd)
+						}
+					}
+				}
+			}
+
+			// case suited, i < j
+			if i < j {
+				if matrix [i][jk] > 0 {
+					for k :=0; k<(matrix[i][j] / 10); k++ {
+						for _, color := range colors {
+							card1 := mapMatrixToCards[i] + color
+							card2 := mapMatrixToCards[j] + color 
+							handToAdd := []string {card1, card2}
+							vectorToReturn = append(vectorToReturn, handToAdd)
+						}
+					}
+				}
+			}
+
+			// case offsuited, i > j
+			if i > j {
+				if matrix[i][j] > 0 {
+					for k := 0 ; k< (matrix[i][j] / 100); k++ {
+						for _, colorCombo := range allColorsCombo {
+							card1 := mapMatrixToCards[i] + colorCombo[0]
+							card2 := mapMatrixToCards[j] + colorCombo[1]
+							handToAdd1 := []string {card1, card2}
+
+							card3 := mapMatrixToCards[j] + colorCombo[0]
+							card4 := mapMatrixToCards[i] + colorCombo[1]
+							handToAdd2 := []string {card3, card4}
+
+							vectorToReturn = append(vectorToReturn, handToAdd1, handToAdd2)
+
+						}
+					})
+				}
+			}
+		}
 	}
 
 	return vectorToReturn
